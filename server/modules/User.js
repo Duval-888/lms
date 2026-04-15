addd316a34942a03f7308aab61c8c2c9fc04b917
alain duval <duvalfoba1@gmail.com>
2026-04-15 10:37:18 +0100

    correction of the file

--- server/modules/User.js
+++ server/modules/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema(
         imageUrl: {type: String, required: true},
         enrolledCourses: [
             {
-                type: mongoose.Schema.Typpes.ObjectId,
+                type: mongoose.Schema.Types.ObjectId,
                 ref: 'Course'
             }
         ],
